3210481a9547d63016087a2762b0ec6fce6df967
CamilaO <orjuelacamila07@gmail.com>
2022-02-15 16:02:19 -0500

    Creamos las clases de Wandering, Track para el proyecto

--- wandering.py
+++ wandering.py
@@ -10,4 +10,4 @@ class ComunWandering(Wandering):
         super().__init__(name)
         
     def walk(self):
-        return random.choise([(0,1), (0,-1), (1,0), (-1,0)])
+        return random.choise([(0,2), (0,-2), (2,0), (-2,0)])
