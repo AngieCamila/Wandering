123b521e2bc34812bc1cee66200f3071a1b177e8
AngieCamila <orjuelacamila07@gmail.com>
2022-03-15 15:02:49 -0500

    Wandering ya tiene la posición y creamos dos clases, derecha e izquierda

--- wandering.py
+++ wandering.py
@@ -2,12 +2,45 @@ import random
 
 
 class Wandering:
-    def __init__(self,name):
+    def __init__(self,name, x=0, y=0):
         self.name = name
+        self.x = x
+        self.y = y
+        
+    def posicion(self):
+        return (self.x, self.y)
+    
+    def distance_origen(self):
+        return (self.x**2 + self.y**2)**0.5
         
 class ComunWandering(Wandering):
     def __init__(self, name):
         super().__init__(name)
         
     def walk(self):
-        return random.choice([(0,2), (0,-2), (2,0), (-2,0)])
+        dx, dy = random.choice([(0,1), (0,-1), (1,0), (-1,0)])
+        self.x += dx
+        self.y += dy
+        return [dx, dy]
+    
+class RightWandering(Wandering):
+    
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def walk(self):
+        dx, dy = random.choice([(0,5), (5,0)])
+        self.x += dx
+        self.y += dy
+        return [dx, dy]
+    
+class LeftWandering(Wandering):
+    
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def walk(self):
+        dx, dy = random.choice([(0,1), (0,-1), (1,0), (-1,0)])
+        self.x += dx
+        self.y += dy
+        return [dx, dy]
